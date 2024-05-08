46e258115b08d11cd71db9700f63fcf9a7c439cb
nikunj Rohit <nikunjrohit10@gmail.com>
2024-05-08 09:58:18 +0000

    add dropdown design

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import './dropdown.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-
+import React, { useState } from "react";
+import "./dropdown.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import editDrop from "../assets/edit-drop.svg";
+import removeDrop from "../assets/remove-drop.svg";
+import shareDrop from "../assets/share-drop.svg";
+import collapesDrop from "../assets/collapes-drop.svg";
 const Dropdown = () => {
   const [showMenu, setShowMenu] = useState(false);
-console.log("showMenu -",showMenu)
+  console.log("showMenu -", showMenu);
   const showDropdown = () => {
     setShowMenu(!showMenu);
   };
@@ -14,11 +17,28 @@ console.log("showMenu -",showMenu)
     <div className="header">
       <div className="dropdown">
         <div className="dropbtn icons" onClick={showDropdown}>
-            <FontAwesomeIcon icon={faEllipsisV} color="black" />
+          <FontAwesomeIcon icon={faEllipsisV} color="black" />
         </div>
-        <div id="myDropdown" className={`dropdown-content ${showMenu ? 'show' : ''}`}>
-          <a href="#home">Open</a>
-          <a href="#about">Edit</a>
+        <div
+          id="myDropdown"
+          className={`dropdown-content ${showMenu ? "show" : ""}`}
+        >
+          <a href="#Edit">
+            <span>Edit</span>
+            <img src={editDrop} alt="Edit" />
+          </a>
+          <a href="#Courier">
+            <span>Courier info</span>
+            <img src={collapesDrop} alt="Share" />
+          </a>
+          <a href="#Share">
+            <span>Share</span>
+            <img src={shareDrop} alt="Share" />
+          </a>
+          <a href="#Remove">
+            <span>Remove</span>
+            <img src={removeDrop} alt="Remove" />
+          </a>
         </div>
       </div>
     </div>
